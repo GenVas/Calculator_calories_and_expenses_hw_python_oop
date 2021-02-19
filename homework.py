b0f057a9a0611b9eac4000c4e8bff0df2dae4d1b
Gennady <vasilievga@yandex.ru>
2021-02-19 23:53:15 +0300

    Спринт2_Калькулятор_ревью3

--- homework.py
+++ homework.py
@@ -14,19 +14,17 @@ class Calculator:
         print(self.limit)
 
     def get_today_stats(self):
-        now_time = dt.date.today()
+        today_time = dt.date.today()
         return sum(record.amount
                    for record in self.records
-                   if record.date == now_time)
+                   if record.date == today_time)
 
     def get_week_stats(self):
-        if not self.records:
-            return "Записей нет"
         time_today = dt.date.today()
         time_7_days_back = time_today - self.TIME_DELTA_7_DAYS
         return sum(record.amount for record
                    in self.records
-                   if time_7_days_back <= record.date <= time_today)
+                   if time_7_days_back < record.date <= time_today)
 
 
 class Record:
@@ -35,53 +33,50 @@ class Record:
     def __init__(self, amount, comment, date=None):
         self.amount = amount
         self.comment = comment
-        self.date = date
-        if self.date is None:
+        if date is None:
             self.date = dt.datetime.now().date()
         else:
             self.date = dt.datetime.strptime(date, self.TIME_FORMAT).date()
 
 
 class CaloriesCalculator(Calculator):
-    POSITIVE_BALANCE = ("Сегодня можно съесть что-нибудь ещё, "
-                        "но с общей калорийностью не более {remainder} кКал")
-    NEGATIVE_BALANCE = "Хватит есть!"
+    NEGATIVE = "Хватит есть!"
+    POSITIVE = ("Сегодня можно съесть что-нибудь ещё, "
+                "но с общей калорийностью не более {remainder} кКал")
 
     def get_calories_remained(self):
-        self.remainder = self.limit - self.get_today_stats()
-        return (self.POSITIVE_BALANCE.format(remainder=self.remainder)
-                if self.limit > self.get_today_stats() else
-                self.NEGATIVE_BALANCE)
+        limit, today_stats = self.limit, self.get_today_stats()
+        remainder = limit - today_stats
+        return (self.NEGATIVE if limit <= today_stats
+                else self.POSITIVE.format(remainder=remainder))
 
 
 class CashCalculator(Calculator):
     USD_RATE = 60.00
     EURO_RATE = 70.00
-    currencies = {'usd': [USD_RATE, 'USD'],
+    CURRENCIES = {'usd': [USD_RATE, 'USD'],
                   'eur': [EURO_RATE, 'Euro'],
                   'rub': [1.00, 'руб']}
     SURPLUS = "На сегодня осталось {money} {name}"
     BALANCE = "Денег нет, держись"
-    DEFICIT = (f"{BALANCE}: твой долг - "
+    DEFICIT = ("Денег нет, держись: твой долг - "
                "{money} {name}")
-    KEY_ERROR_CURRENCY = ("Указанный Вами код валюты - \"{name}\""
-                          " - отсутствует в системе.\n"
-                          "Исправьте и повторите запрос, пожалуйста.")
+    ERROR_CURRENCY = ("currency input \"{name}\""
+                      " is not in CURRENCIES")
 
     def get_today_cash_remained(self, currency):
-        try:
-            rate, name = self.currencies[currency]
-        except KeyError:
-            return self.KEY_ERROR_CURRENCY.format(name=currency)
-        benchmark = self.get_today_stats()
-        if self.limit == benchmark:
+        today_stats, limit = self.get_today_stats(), self.limit
+        if limit == today_stats:
             return self.BALANCE
-        base_amount = self.limit - self.get_today_stats()
-        self.remainder = round(base_amount / rate, 2)
-        if self.limit > benchmark:
-            return self.SURPLUS.format(money=self.remainder,
+        if currency not in self.CURRENCIES:
+            raise ValueError(self.ERROR_CURRENCY.format(name=currency))
+        rate, name = self.CURRENCIES[currency]
+        base_amount = limit - today_stats
+        remainder = round(base_amount / rate, 2)
+        if limit > today_stats:
+            return self.SURPLUS.format(money=remainder,
                                        name=name)
-        return self.DEFICIT.format(money=-self.remainder,
+        return self.DEFICIT.format(money=-remainder,
                                    name=name)
 
 
@@ -90,7 +85,7 @@ if __name__ == "__main__":
     r1 = Record(amount=145, comment='Безудержный шопинг', date='08.03.2019')
     cash_calculator.add_record(Record(amount=200,
                                       comment='бар в Танин др',
-                                      date='15.02.2021'))
+                                      date='12.02.2021'))
     cash_calculator.add_record(Record(amount=700, comment='кофе'))
     cash_calculator.add_record(Record(amount=500, comment='Серёге за обед'))
     cash_calculator.add_record(Record(amount=500,
@@ -100,15 +95,15 @@ if __name__ == "__main__":
     print(cash_calculator.get_today_cash_remained('rub'))
     print(cash_calculator.get_week_stats())
     # Проверка на выходные данные Калокулятора калорий:
-    calories_calculator = CaloriesCalculator(2000)
+    calories_calculator = CaloriesCalculator(500)
     calories_calculator.add_record(Record(amount=201,
                                    comment='яблоко',
                                    date='01.02.2021'))
     calories_calculator.add_record(Record(amount=200,
                                    comment='морковь',
                                    date='15.02.2021'))
-    calories_calculator.add_record(Record(amount=101,
+    calories_calculator.add_record(Record(amount=201,
                                    comment='суп'))
-    # print(calories_calculator.get_today_stats())
-    # print(calories_calculator.get_calories_remained())
-    # print(calories_calculator.get_week_stats())
+    print(calories_calculator.get_today_stats())
+    print(calories_calculator.get_calories_remained())
+    print(calories_calculator.get_week_stats())
